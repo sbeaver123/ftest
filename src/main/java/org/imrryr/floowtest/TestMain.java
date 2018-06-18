591c6ff7ef09b15d02d8e2f0f446110c66553da7
Simon <github@imrryr.org.uk>
2018-06-18 12:31:28 +0100

    Add command line argument handling for filename and MongoDB url

--- src/main/java/org/imrryr/floowtest/TestMain.java
+++ src/main/java/org/imrryr/floowtest/TestMain.java
@@ -3,6 +3,12 @@
  */
 package org.imrryr.floowtest;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 
@@ -18,10 +24,29 @@ public class TestMain {
 	 */
 	public static void main(String[] args) {
 
-		MongoClientURI uri = new MongoClientURI("mongodb://localhost:27017");
-		MongoClient mongo = new MongoClient(uri);
+		Options options = new Options();
+		options.addOption("source", true, "The name of the file to be processed");
+		options.addOption("mongo", true, "The URL of the MongoDB server to use <hostname:port>");
 		
-		String filename = "C://Temp/test1.txt";
+		String uri = null;
+		String filename = null;
+
+		CommandLineParser clp = new DefaultParser();
+		try {
+			CommandLine cl = clp.parse(options, args);
+			if (cl.hasOption("source") && cl.hasOption("mongo")) {
+				filename = cl.getOptionValue("source");
+				uri = cl.getOptionValue("mongo");
+			} else {
+				System.out.println("Required options missing");
+				return;
+			}
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		MongoClientURI muri = new MongoClientURI("mongodb://" + uri);
+		MongoClient mongo = new MongoClient(muri);
 		
 		FileProcessor fr = new FileProcessor();
 		fr.readFile(mongo, filename);
